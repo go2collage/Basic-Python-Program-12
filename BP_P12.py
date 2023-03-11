ab17a77922e506c5b716fb766b905c9572162236
go2collage <91822422+go2collage@users.noreply.github.com>
2023-03-11 22:40:11 +0530

    Add files via upload

---
+++ BP_P12.py
@@ -0,0 +1,14 @@
+# Basic Python Program
+
+# Check number is Positive, Negative and Zero
+
+num = float(input("Enter a number: "))
+
+if num < 0:         # if number is less tha Zero(0)
+    print(num," is Negative number.")
+
+elif num > 0:          # if number is greater than Zero(0)
+    print(num," is Positive number.")
+
+else:               # Otherwise number is Zero(0)
+    print(num," is Zero number.")
